437d6157cec377331be25dba097b8dbff82e1538
Bigeng Wang <bw2482@columbia.edu>
2025-09-30 16:05:56 -0700

    Updated again the latest hash for git.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '65951189e723b9e009aad5d35b9811bb27486fe8'
+AESD_ASSIGNMENTS_VERSION = '5fd8b950cc1f899d488c361f970cb94d1bf3b0ed'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
