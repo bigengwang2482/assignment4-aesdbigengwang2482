f7f7e49c253bef9f850ff5607f3a632d923f2882
Bigeng Wang <bw2482@columbia.edu>
2025-09-30 14:42:02 -0700

    Updated the git hash which execution are assuming PATH.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '76db2b54607c89fb7bac70dd0260daf8ddd3ea09'
+AESD_ASSIGNMENTS_VERSION = '65951189e723b9e009aad5d35b9811bb27486fe8'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
